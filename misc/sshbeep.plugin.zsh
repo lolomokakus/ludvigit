03f209ffbdd70e5f45cd248709d0bd6559738f0a
zeroxfourc <ludvighg1999@gmail.com>
2016-03-16 11:29:56 +0100

    sshbeep: Use tone instead of beep (for real this time)

--- misc/sshbeep.plugin.zsh
+++ misc/sshbeep.plugin.zsh
@@ -1,5 +1,5 @@
-# test if beep is installed, we're connected via ssh and if beeping on ssh login is enabled or not
-if [ -n "$(command -v beep)" -a -n "$SSH_CONNECTION" -a ! -x "$HOME/.quickly_and_quietly" ]; then
+# test if tone is installed, we're connected via ssh and if beeping on ssh login is enabled or not
+if [ -n "$(command -v tone)" -a -n "$SSH_CONNECTION" -a ! -x "$HOME/.quickly_and_quietly" ]; then
 	# a nice little chime
 	tone c4 100 e4 400
 fi

d584da35292049be2328d907ffc81714366b51ee
Ludvig Holtze <ludvighg1999@gmail.com>
2016-08-24 18:50:32 +0200

    thanks shellcheck

--- misc/pull.sh
+++ misc/pull.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd "$( dirname "${BASH_SOURCE[0]}" )" # cd into script's directory
+cd "$( dirname "${BASH_SOURCE[0]}" )" || exit # cd into script's directory
 for dir in */ ; do
-	cd $dir # cd into every subdirectory
+	cd $dir || exit # cd into every subdirectory
 	if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1 ; then # is this a git repository?
 		echo ${dir%/}: not a git repository
 		cd .. # go back to parent directory

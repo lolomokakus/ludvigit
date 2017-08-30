91be3fb3369f3aba767dea2c6a4e4d7302a0dafd
zeroxfourc <ludvighg1999@gmail.com>
2017-08-30 20:21:08 +0200

    typewrite.c: test sound

--- src/typewrite.c
+++ src/typewrite.c
@@ -1,18 +1,39 @@
+#include <ctype.h>
+#include <fcntl.h>
+#include <linux/kd.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <time.h>
 int main(int argc, char *argv[]) {
-	int character;
+	bool sound = false;
+	int delayms = 50;
+	int console;
+	char character;
 	struct timespec delay;
 	delay.tv_sec = 0;
-	if(argc < 2) {
-		delay.tv_nsec = 50000000;
-	} else {
-		delay.tv_nsec = atoi(argv[1]) * 1000000;
+	if(argc >= 3) {
+		if(strcmp(argv[2], "sound")) {
+			sound = true;
+		}
+	}
+	if(argc >= 2 && atoi(argv[1]) >= 5) {
+		delayms = atoi(argv[1]);
+	}
+	delay.tv_nsec = (delayms - 5) * 1000000;
+	if(sound) {
+		console = open("/dev/console", O_WRONLY);
 	}
 	while(character = getchar(), character != EOF) {
 		putchar(character);
 		fflush(stdout);
+		if(!isspace(character) && sound) {
+			ioctl(console, KIOCSOUND, 1193);
+			nanosleep((const struct timespec[]){{0, 5000000}}, NULL);
+			ioctl(console, KIOCSOUND, 0);
+		}
 		nanosleep(&delay, NULL);
 	}
 	return 0;

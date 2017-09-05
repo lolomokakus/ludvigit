652dbd29030ba1e79573f1f5d693e1df72f4cb14
zeroxfourc <ludvighg1999@gmail.com>
2017-09-05 16:05:58 +0200

    piano.c: change default key

--- src/piano.c
+++ src/piano.c
@@ -4,12 +4,10 @@
 #include <stdlib.h> // For atoi()
 #include <sys/ioctl.h> // For ioctl()
 #include <time.h> // For nanosleep()
-
-#define A4 440
-
 int main(int argc, char *argv[]) {
+	static const int a4 = 440;
 	int console = open("/dev/tty0", O_WRONLY);
-	int key = 49;
+	int key = 40;
 	struct timespec duration = {1, 0};
 	struct timespec delay = {0, 0};
 	if(argc >= 2) {
@@ -22,7 +20,7 @@ int main(int argc, char *argv[]) {
 	if(argc >= 4) {
 		delay.tv_nsec = atoi(argv[3]) * 1000000;
 	}
-	ioctl(console, KIOCSOUND, (int)(1193180 / (pow(2, (key - 49) / 12) * A4)));
+	ioctl(console, KIOCSOUND, (int)(1193180 / (pow(2, (key - 49) / 12) * a4)));
 	nanosleep(&duration, NULL);
 	ioctl(console, KIOCSOUND, 0);
 	nanosleep(&delay, NULL);

2260b92926e638b00f5bf9bfb1433378742ae9e1
Ludvig Holtze <ludvighg1999@gmail.com>
2017-10-28 22:05:25 +0200

    sus.hs: slightly neater syntax

--- src/sus.hs
+++ src/sus.hs
@@ -3,8 +3,8 @@ import System.IO
 sus :: String -> String
 sus word
 	| length word == 1 = word
-	| length word `mod` 2 == 0 = take (length word `div` 2) word ++ reverse (take (length word `div` 2) word)
-	| otherwise = take (length word `div` 2 + 1) word ++ reverse (take (length word `div` 2) word)
+	| length word `mod` 2 == 0 = take (length word `div` 2) word ++ (reverse . take (length word `div` 2)) word
+	| otherwise = take (length word `div` 2 + 1) word ++ (reverse . take (length word `div` 2)) word
 
 main :: IO ()
 main = do
@@ -13,5 +13,5 @@ main = do
 		then return ()
 		else do
 			line <- hGetLine stdin
-			putStrLn (unwords (map sus (words line)))
+			putStrLn $ (unwords . map sus . words) line
 			main

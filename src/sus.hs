0bd608bbf4a2a8f30f166f85af8706d96766d6aa
Ludvig Holtze <ludvighg1999@gmail.com>
2017-10-29 19:58:00 +0100

    sus.hs: restructure

--- src/sus.hs
+++ src/sus.hs
@@ -1,6 +1,4 @@
-import Control.Monad
 import System.Environment
-import System.Exit
 import System.IO
 
 sus :: String -> String
@@ -9,17 +7,22 @@ sus word
 	| length word `mod` 2 == 0 = take (length word `div` 2) word ++ (reverse . take (length word `div` 2)) word
 	| otherwise = take (length word `div` 2 + 1) word ++ (reverse . take (length word `div` 2)) word
 
+susArgs :: [String] -> IO ()
+susArgs args = putStrLn $ (unwords . map sus . words . unwords) args
+
+susStdin :: IO ()
+susStdin = do
+	eof <- hIsEOF stdin
+	if eof
+		then return ()
+		else do
+			line <- getLine
+			putStrLn $ (unwords . map sus . words) line
+			susStdin
+
 main :: IO ()
 main = do
 	args <- getArgs
-	if (not . null) args
-		then do
-			putStrLn $ (unwords . map sus . words . unwords) args
-			exitSuccess
-		else forever $ do
-			eof <- hIsEOF stdin
-			if eof
-				then exitSuccess
-				else do
-					line <- getLine
-					putStrLn $ (unwords . map sus . words) line
+	if null args
+		then susStdin
+		else susArgs args

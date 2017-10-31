6d3642b67bf0f22576a22f734722299170891984
Ludvig Holtze <ludvighg1999@gmail.com>
2017-10-31 11:59:30 +0100

    sus.hs: make even neater

--- src/sus.hs
+++ src/sus.hs
@@ -1,4 +1,3 @@
-import System.Environment
 import System.IO
 
 sus :: String -> String
@@ -7,22 +6,5 @@ sus word
 	| length word `mod` 2 == 0 = take (length word `div` 2) word ++ (reverse . take (length word `div` 2)) word
 	| otherwise = take (length word `div` 2 + 1) word ++ (reverse . take (length word `div` 2)) word
 
-susArgs :: [String] -> IO ()
-susArgs args = putStrLn $ (unwords . map sus . words . unwords) args
-
-susStdin :: IO ()
-susStdin = do
-	eof <- hIsEOF stdin
-	if eof
-		then return ()
-		else do
-			line <- getLine
-			putStrLn $ (unwords . map sus . words) line
-			susStdin
-
 main :: IO ()
-main = do
-	args <- getArgs
-	if null args
-		then susStdin
-		else susArgs args
+main = interact (unlines . map (unwords . map sus . words) . lines)

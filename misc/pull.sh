3361943dd08019c1b0ebc045948ef6d199b302da
Ludvig Holtze <ludvighg1999@gmail.com>
2017-12-31 19:35:57 +0100

    pull.sh: mer svenska

--- misc/pull.sh
+++ misc/pull.sh
@@ -3,7 +3,7 @@ cd "$(dirname $0)" # Gå till katalogen som skriptet ligger i
 for dir in */ ; do
 	cd $dir # Gå in i alla underkataloger
 	if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1 ; then # Försäkra dig om att katalogen innehåller ett git-repo
-		echo ${dir%/}: not a git repository
+		echo ${dir%/}: ingen gitkatalog
 		cd .. # Gå tillbaka till den övre katalogen
 		continue
 	fi
